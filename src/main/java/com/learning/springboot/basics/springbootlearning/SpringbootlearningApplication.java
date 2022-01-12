1fde99765ff258a46138681cce2f89707624a556
santhosh yamsani <yasoda.yks@gmail.com>
2022-01-12 13:18:52 +0530

    second commit

---
+++ src/main/java/com/learning/springboot/basics/springbootlearning/SpringbootlearningApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.basics.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootlearningApplication.class, args);
+	}
+
+}
